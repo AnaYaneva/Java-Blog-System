d879de15722d0d6a9d838e16078a53c0885a899d
Ana Yaneva <ana_yaneva@abv.bg>
2018-01-30 10:48:46 +0200

    Add files via upload
    Users and Admin panels done

---
+++ src/main/java/blogSystem/controller/HomeController.java
@@ -0,0 +1,36 @@
+package blogSystem.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import blogSystem.entity.Article;
+import blogSystem.repository.ArticleRepository;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @GetMapping("/")
+    public String index(Model model) {
+
+        List<Article> articles = this.articleRepository.findAll();
+
+        model.addAttribute("view", "home/index");
+        model.addAttribute("articles", articles);
+
+        return "base-layout";
+    }
+
+    @RequestMapping("/error/403")
+    public String accessDenied(Model model){
+        model.addAttribute("view", "error/403");
+
+        return "base-layout";
+    }
+}
